f99c821223163e10768983cda166ad26d8958633
Lis Louise <llouise@exocad.com>
2024-03-14 19:42:59 +0100

    env variable corrected

--- app/components/BookingForm.js
+++ app/components/BookingForm.js
@@ -1,8 +1,7 @@
 // components/BookingForm.js
 export default function BookingForm() {
-  const googleFormUrl = process.env.GOOGLE_FORM;
-  console.log(googleFormUrl)
-  console.log(process.env.GOOGLE_FORM)
+  const googleFormUrl = process.env.NEXT_PUBLIC_GOOGLE_FORM;
+
   return (
     <div className="iframe-container">
       <iframe src={googleFormUrl} title="Painting Form" frameborder="0" width="100%" height="520"></iframe>
