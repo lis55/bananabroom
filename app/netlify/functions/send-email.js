b912be58d92c15120af2bd2f62106a39430c430f
Lis Louise <llouise@exocad.com>
2024-07-27 16:45:25 +0200

    catch error in lambda function

--- app/netlify/functions/send-email.js
+++ app/netlify/functions/send-email.js
@@ -1,28 +1,40 @@
 const nodemailer = require('nodemailer');
 
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_ID_SECRET,
+  },
+});
+
 exports.handler = async (event) => {
-  let data = JSON.parse(event.body);
+  try {
+    let data = JSON.parse(event.body);
 
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        type: 'OAuth2',
-        user: process.env.EMAIL,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_ID_SECRET,
-      },
-  });
+    console.log('Sending email to:', data.to);
 
-  await transporter.sendMail({
-    from:process.env.EMAIL,
-    to: data.to,
-    subject: data.subject,
-    text: data.text,
-    html: data.html,
-  });
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: data.to,
+      subject: data.subject,
+      text: data.text,
+      html: data.html,
+    });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Welcome to BananaBroom... We make your space shine" })
-  };
-}
+    console.log('Email sent successfully');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Welcome to BananaBroom... We make your space shine" }),
+    };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
